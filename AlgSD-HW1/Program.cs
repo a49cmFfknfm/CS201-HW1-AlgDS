f6bee884b2178b3cad61e308d23098576f347f7a
a49cmFfknfm <serhii.serdiul.25@kse.org.ua>
2026-03-13 19:43:43 +0000

    Initial commit

---
+++ AlgSD-HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿while (true){
+    Console.WriteLine("Provide a number to choose what you want to do next:\n1. Provide math problem and solve");
+    var usParmInp = Console.ReadLine();
+    switch (usParmInp)
+    {
+        case ("1"):
+            Console.WriteLine("FFGGFGFFG");
+            string problem = Console.ReadLine();
+            tokenize(problem);
+            break;
+        default:
+            Console.WriteLine("Not a valid option");
+            break;
+    }
+
+}
---
+++ AlgSD-HW1/Tokenizer.cs
@@ -0,0 +1,12 @@
+namespace AlgSD_HW1;
+
+public class Tokenizer
+{
+    public Tokenizer(){}
+
+    public string Tokenize(string data)
+    {
+        for () ;
+
+    }
+}
